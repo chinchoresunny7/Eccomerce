a8a7a41beb5cb3969385061668ec886463df0255
Sunny <sunnychinchore7@gmail.com>
2025-07-09 13:09:33 +0530

    Integrated Extent Reports.

--- src/test/java/Frameworks/Tests/ErrorValidation.java
+++ src/test/java/Frameworks/Tests/ErrorValidation.java
@@ -25,7 +25,7 @@ public class ErrorValidation extends baseTest {
     public Object[][] loginDataProvider() {
         return new Object[][]{
                 {"Punu@xyz.com", "Abc@123"},
-                {"Punu@xyz.co", "Abc@123"},
+                {"Punu@xyz.co", "Abc@1234"},
                 {"Pnu@xyz.com", "bc@1234"}
         };
     }
--- src/test/java/Frameworks/testComponents/baseTest.java
+++ src/test/java/Frameworks/testComponents/baseTest.java
@@ -1,10 +1,10 @@
 package Frameworks.testComponents;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -53,6 +53,14 @@ public class baseTest {
         return data;
 
 
+    }
+
+    public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir") + "//Reports//" + testCaseName +".png");
+        FileUtils.copyFile(src,file);
+        return System.getProperty("user.dir") + "//Reports//" + testCaseName +".png";
     }
 }
 
